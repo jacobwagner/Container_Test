ce90c60809a94dc58afda04357ae9cd155d16ee4
Jenkins <jenkins@propter.net>
2016-07-05 14:24:54 +0000

    git ignore

--- cmd/paramicoWrap.py
+++ cmd/paramicoWrap.py
@@ -8,12 +8,5 @@ class Paramiko:
 			stdin, stdout, stderr = ssh.exec_command(command)
 			resList = []
 			for line in stdout.readlines():
-				resList.append(i.encode('ascii', 'ignore'))
-			return resList 
-		except Exception as inst:
-			print "generateList error"
-			print type(inst)   # the exception instance
-			print inst.args     # arguments stored in .args
-
-			
+				
 
